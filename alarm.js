32a4b576089c8b26f1c8d1606f09396fba2ed675
Jibrin-paul Esther <154056941+JibrintheMLevangelist@users.noreply.github.com>
2024-03-30 21:15:27 +0000

    Update alarm.js

--- alarm.js
+++ alarm.js
@@ -1,73 +1,87 @@
-const currentTime=document.querySelector("h1"),
-content=document.querySelector(".content"),
-selectMenu=document.querySelectorAll("select"),
-setAlarmBtn=document.querySelector("button"),
+document.addEventListener('DOMContentLoaded', () => {
 
-let alarmTime, isAlarmSet=false,
-// ringtone=new Audio("./files")
+	const currentTime=document.querySelector("h1");
+	const content=document.querySelector(".content");
+	const hourSelect=document.getElementById("hour");
+	const minuteSelect=document.getElementById("minute");
+	const secondSelect=document.getElementById("second");
+	const ampmSelect=document.getElementById("ampm");
+	const setAlarmBtn=document.querySelector("button"); 
+	const alarmTimeDisplay = document.getElementById('alarmTimeDisplay');
 
-for(let i=12; i>0; i--)
-{
-	i=i<10? "0" +i:i;
-	let option=<option value="${i}">${i}</option>;
-	selectMenu[0].firstElementChild.insertAdjacentHTML("afterend",option);
-}
-for(let i=59; i>0; i--)
-{
-	i=i<10? "0" +i:i;
-	let option=<option value="${i}">${i}</option>;
-	selectMenu[1].firstElementChild.insertAdjacentHTML("afterend",option);
-}
-for(let i=2; i>0; i--)
-{
-	let ampm= i==1? "AM" :"PM";
-	i=i<10? "0" +i:i;
-	let option=<option value="${ampm}">${ampm}</option>;
-	selectMenu[2].firstElementChild.insertAdjacentHTML("afterend",option);
-}
-setInterval(()=>
-{
-	let date=new Date(),
-	h = date.getHours(),
-	m = date.getMinutes(),
-	s = date.getSeconds(),
-	ampm= "AM";
-	if(h>=12)
+	let alarmTime, isAlarmSet=false,
+	ringtone=new Audio("ringtone.mp3");
+
+	for(let i=1; i<=12; i++)
 	{
-		h=h-12;
-		ampm="PM";
+		let formattedHour = i<10? `0${i}` :i;
+		hourSelect.innerHTML += `<option value="${formattedHour}">${formattedHour}</option>`;
 	}
-	h=h==0? h=12:h;
-	h=h<10?"0"+h:h;
-	m=m<10?"0"+m:m;
-	s=s<10?"0"+s:s;
+	for(let i=1; i<=59; i++)
+	{
+		let formattedMinute = i<10? `0${i}` :i;  
+		minuteSelect.innerHTML += `<option value="${formattedMinute}">${formattedMinute}</option>`;
+	}
 	
-	currentTime.innerText='${h}:${m}:${ampm}';
-	if(alarmTime=='${h}:${m}:${ampm}')
-		{
-			ringtone.play();
-			ringtone.loop=true;
+	["AM" , "PM"].forEach(ampm =>
+	{
+		ampmSelect.innerHTML += `<option value="${ampm}">${ampm}</option>`;
+	});
+	
+	setInterval(()=>
+	{
+		let date=new Date(),
+		h = date.getHours(),
+		m = date.getMinutes(),
+		s = date.getSeconds(),
+		ampm= h >=12? 'PM' : 'AM';
+
+		h=h%12;
+		h=h? h:12;
+		h=h<10?"0"+h:h;
+		m=m<10?"0"+m:m;
+		s=s<10?"0"+s:s;
+		
+		const timestring=`${h}:${m}:${s} ${ampm}`;
+		currentTime.innerText=timestring;
+		if(alarmTime==timestring && isAlarmSet && !ringtone.loop)
+			{
+				ringtone.play();
+				ringtone.loop=true;
+			}
+			},1000);
+	function setAlarm()
+	{
+		let hourSelectValue = hourSelect.value;
+		let minuteSelectValue = minuteSelect.value;
+		let ampmSelectValue = ampmSelect.value;
+		
+
+		if(isAlarmSet){
+			alarmTime="";
+			ringtone.pause();
+			ringtone.loop = false;
+			content.classList.remove("disable");
+			setAlarmBtn.innerText="Set Alarm";
+			alarmTimeDisplay.innerText="Alarm not set";
+			isAlarmSet=false;
 		}
-		},1000);
-function setAlarm()
-{
-	if(isAlarmSet){
-		alarmTime="";
-		ringtone.pause();
-		content.classList.remove("disable");
-		setAlarmBtn.innerText="Set Alarm";
-		return isAlarmSet=false;
+		else
+		{
+			let time=`${hourSelect.value}:${minuteSelect.value} ${ampmSelect.value}`
+			if(!hourSelectValue||!minuteSelectValue || !ampmSelectValue)
+			{
+				alert("Select a valid time to set the alarm!");
+				return;
+			}
+			alarmTime=time;
+			isAlarmSet=true;
+			content.classList.add("disable");
+			setAlarmBtn.innerText="Clear Alarm";
+			alarmTimeDisplay.innerText = `Alarm Set For: ${alarmTime} `;
+		}
 	}
-	let time=${selectMenu[0].value}:${selectMenu[1].value} ${selectMenu[2].value}
-if(time.includes("Hour")||time.includes("Minute") || time.includes("AM/PM"))
-{
-	return.alert("Select a valid time to set the alarm!");
-}
-isAlarmSet=true;
-alarmTime=time;
-	content.classList.add("disable");
-	setAlarmBtn.innerText="Clear Alarm";
-}
-	setALarmBtn.addEventListener("click",setAlarm);
-	
-	
+		setAlarmBtn.addEventListener("click",setAlarm);
+		
+});	
+
